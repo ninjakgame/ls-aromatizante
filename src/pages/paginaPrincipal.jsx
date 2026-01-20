8eaf38bbbc37ecf81663e58ff454105aee1c989b
ninjakgame <girlanesbn@gmaail.com>
2026-01-19 23:11:23 -0300

    Exclui a api para depois fazer

--- src/pages/paginaPrincipal.jsx
+++ src/pages/paginaPrincipal.jsx
@@ -1,16 +1,15 @@
 import Logo from "../img/logo_trial-removebg-preview.png";
 import IconePerfil from "../img/image 22.svg";
 import IconeCarinho from "../img/image 23.svg";
-import Banner from "../img/Design sem nomePix.gif";
 import style from "../css/paginaPrincipal.module.css";
-import IconeProduto1 from "../img/10Pix.jpg"
+import IconeCartao from "../img/PAGAMENTO.svg"
 
 function Logado() {
   
   return (
     <div className={style.container}>
 
-      <header className={style.header}>
+      <div className={style.header}>
         <img src={Logo} className={style.logo} alt="Logo Trial" />
 
         <nav className={style.navegador}>
@@ -23,38 +22,12 @@ function Logado() {
           <img src={IconeCarinho} alt="Carrinho" />
           <img src={IconePerfil} alt="Perfil" />
         </div>
-      </header>
+  </div>
 
-      <main>
-        <div className={style.banner}>
-          <img
-            src={Banner}
-            className={style.gifBanner}
-            alt="Banner Promocional"
-          />
-
-          <div className={style.containerPromotion}>
-            <h2 className={style.promotion}>PROMOÇÃO</h2>
-            <p>Aromatizante de Casa <br /> Sabor: Lavanda</p>
-
-            <div className={style.botoes}>
-              <button className={style.botoesRegistro}>Comprar Agora</button>
-              <button className={style.botoesVer}>Ver Mais</button>
-            </div>
-          </div>
-
-          <div className={style.produtos}>
-              <p>Destaque</p>
-
-              <div className={style.tiposProdutos}>
-                  <div className={style.imagemProduto}>
-                      <img src={IconeProduto1} className={style.ImagemProduct} alt="" />
-                  </div>
-              </div>
-          </div>
+  <div>
+          <img src={IconeCartao} alt="" />
         </div>
-      </main>
-    </div>
+  </div>
   );
 }
 
